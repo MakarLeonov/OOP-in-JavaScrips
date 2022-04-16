9383739845d002fd3a1e44659bbe654eaec8affa
unknown <MakarLeonov>
2022-04-16 15:45:54 +0300

    Added a graphical interface.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,48 @@
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', ()=>{
+    let input_x1 = document.querySelector('#x1');
+    let input_y1 = document.querySelector('#y1');
+    let input_x2 = document.querySelector('#x2');
+    let input_y2 = document.querySelector('#y2');
+
+    let triangle_1 = new Triangle(input_x1, input_y1, input_x2, input_y2);
+    triangle_1.findeTheDistance();
+})
+
 class Triangle {
-    constructor(x1, y1, x2, y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+    constructor(input_x1, input_y1, input_x2, input_y2) {
+        this.x1 = input_x1.value;
+        this.y1 = input_y1.value;
+        this.x2 = input_x2.value;
+        this.y2 = input_y2.value;
     }
     findeTheDistance(){
+
+        console.log(this.x1);
+        console.log(this.x2);
         let katet1;
         if ((this.x1 > 0 & this.x2 < 0) || (this.x2 > 0 & this.x1 < 0)) {
             katet1 = Math.abs(this.x1) + Math.abs(this.x2);
         } else {
-            if (this.x1 > this.x2)
-                katet1 = this.x1 - this.x2;
-            else 
-                katet1 = this.x2 - this.x1;
+            if (this.x1 > this.x2){
+                katet1 = Math.abs(this.x1 - this.x2)
+            } else {
+                katet1 = Math.abs(this.x2 - this.x1);
+            }
         }
-        console.log(katet1);
-
+        
         let katet2;
         if ((this.y1 > 0 & this.y2 < 0) || (this.y2 > 0 & this.y1 < 0)) {
             katet2 = Math.abs(this.y1) + Math.abs(this.y2);
         } else {
             if (this.y1 > this.y2)
-                katet2 = this.y1 - this.y2;
+                katet2 = Math.abs(this.y1 - this.y2);
             else 
-                katet2 = this.y2 - this.y1;
+                katet2 = Math.abs(this.y2 - this.y1);
         }
-        console.log(katet2);
 
-        console.log(Math.sqrt(Math.pow(katet1, 2) + Math.pow(katet2, 2)).toFixed(2));
+        // console.log(Math.sqrt(Math.pow(katet1, 2) + Math.pow(katet2, 2)).toFixed(2)); - для вывода в консоль
+
+        document.querySelector('.result').innerText = Math.sqrt(Math.pow(katet1, 2) + Math.pow(katet2, 2)).toFixed(2);
     }
 }
-
-let triangle_1 = new Triangle(-2, -7, -7, 1);
-triangle_1.findeTheDistance();
